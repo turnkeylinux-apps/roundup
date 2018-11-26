805505748f480e5146aa64467721aa912325aba9
Jeremy Davis <jeremy@turnkeylinux.org>
2018-11-26 18:27:23 +1100

    inithook: set domain; apache conf: redirect to https - support for absolute url in roundup

--- overlay/usr/lib/inithooks/bin/roundup.py
+++ overlay/usr/lib/inithooks/bin/roundup.py
@@ -4,7 +4,8 @@
 Option:
     --pass=     unless provided, will ask interactively
     --email=    unless provided, will ask interactively
-
+    --domain=   unless provided, will ask interactively
+                DEFAULT=www.example.com
 """
 
 import os
@@ -15,6 +16,7 @@ import hashlib
 
 from dialog_wrapper import Dialog
 from mysqlconf import MySQL
+from executil import system
 
 def usage(s=None):
     if s:
@@ -23,15 +25,18 @@ def usage(s=None):
     print >> sys.stderr, __doc__
     sys.exit(1)
 
+DEFAULT_DOMAIN="www.example.com"
+
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'pass=', 'email='])
+                                       ['help', 'pass=', 'email=', 'domain='])
     except getopt.GetoptError, e:
         usage(e)
 
     password = ""
     email = ""
+    domain = ""
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
@@ -39,6 +44,8 @@ def main():
             password = val
         elif opt == '--email':
             email = val
+        elif opt == '--domain':
+            domain = val
 
     if not password:
         d = Dialog('TurnKey Linux - First boot configuration')
@@ -57,11 +64,33 @@ def main():
 
     inithooks_cache.write('APP_EMAIL', email)
 
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "Roundup Domain",
+            "Enter the domain to serve Roundup.",
+            DEFAULT_DOMAIN)
+
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
+    inithooks_cache.write('APP_DOMAIN', domain)
+
     hashpass = "{SHA}" + hashlib.sha1(password).hexdigest()
 
     m = MySQL()
     m.execute('UPDATE roundup._user SET _address=\"%s\" WHERE _username=\"admin\";' % email)
     m.execute('UPDATE roundup._user SET _password=\"%s\" WHERE _username=\"admin\";' % hashpass)
+
+    conf = "/etc/roundup/tracker-config.ini"
+    system("sed -i \"s|^web =.*|web = https://%s/|\" %s" % (domain, conf))
+
+    apache_conf = "/etc/apache2/sites-available/roundup.conf"
+    system("sed -i \"\|RewriteRule|s|https://.*|https://%s/\$1 [R,L]|\" %s" % (domain, apache_conf))
+    system("sed -i \"\|RewriteCond|s|!^.*|!^%s$|\" %s" % (domain, apache_conf))
+
     os.system('service apache2 restart')
     
 
